605af59540f32ef1675197339ed7c1c4de77fea8
Wesley <wesleyrg2000@gmail.com>
2018-05-16 11:55:18 -0400

    mudança de repo

--- Funções Python/integer_and_ float.py
+++
@@ -1,6 +0,0 @@
-# https://docs.python.org/3/library/stdtypes.html#text-sequence-type-str
-Nove = 9
-
-bin(Nove)                           # 0b1001
-Nove.bit_length()                   # 4  # ou (9).bit_loength()
-Nove.to_bytes(2, byteorder='big')   # b'\x00\t'
